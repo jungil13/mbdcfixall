e1b71f14b090b5035d3a411565941e9aec11f885
jungil13 <jungilcasquejo5@gmail.com>
2026-09-25 11:26:39 +0800

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mbdcdixall.app',
+  appName: 'mbdcfixall',
+  webDir: 'public'
+};
+
+export default config;
